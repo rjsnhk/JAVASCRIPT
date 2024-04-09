88e2e82fc924d768880d9ff18439fbc3ec3cbf6f
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-04-09 18:52:25 +0530

    Add files via upload

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,23 @@
+const accountId=39;
+let accountEmail="rn@kfkkf";
+var accPassword="rjsnhk";
+
+acountCity="odisha";
+
+let accStatus;
+
+//const change nhi hoga kabhi
+
+// accountId=2;
+
+//use let and const
+
+
+accountEmail="siz@mmkf";
+accPassword="sunathadrgb=bnm";
+acountCity="kashmir";
+console.log(accountId);
+
+
+
+console.table([accountId,accountEmail,accPassword,acountCity,accStatus]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as new version;
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3+3)
+console.log("raj")
+
+let name="raj";
+let age=26;
+let login=true
+
+//number
+//bigint
+
+//string =>""
+//boolean =>true/false
+//null=>standalone value  let temp=null;
+//undefined=>  let temp;
+
+//symbol =>unique
+
+//object
+console.log(typeof null) //object
+console.log(typeof age)//number
+
+console.log(typeof "hii");
