b3eb75c562eaf377b86e29e64da80716fa75b0de
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2024-04-09 22:33:03 +0530

    Add files via upload

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "rajesh "
+const repoCount = 21
+
+// console.log(name + repoCount + " nahak");
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('raj-esh-na-hak')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('e'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7,5);
+// console.log(anotherString);
+
+// const newStringOne = "   rajesh nadkjakf   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://rajesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+console.log(url.includes('.com')) //available
+
+console.log(gameName.split('-'));
